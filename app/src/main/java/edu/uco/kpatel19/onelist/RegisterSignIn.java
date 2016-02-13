31284d0b2208da3c005130beaffb80918bb2e4a3
Kaush2190 <kaushalpatel2190@gmail.com>
2016-02-13 17:20:19 -0600

    Initial commit

---
+++ app/src/main/java/edu/uco/kpatel19/onelist/RegisterSignIn.java
@@ -0,0 +1,14 @@
+package edu.uco.kpatel19.onelist;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class RegisterSignIn extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_sign_in);
+    }
+
+}
